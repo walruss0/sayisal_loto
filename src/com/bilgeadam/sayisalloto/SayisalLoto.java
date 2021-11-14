17687ac877b62b871ef8a51d6b4b49f91bb339a5
Onur Yildirim <onuryildirim5425@gmail.com>
2021-11-14 14:46:06 +0300

    SAyisal Loto

--- src/com/bilgeadam/sayisalloto/SayisalLoto.java
+++ src/com/bilgeadam/sayisalloto/SayisalLoto.java
@@ -7,7 +7,7 @@ public class SayisalLoto {
         Random rnd = new Random();
         return rnd.nextInt(maxSayi+1);
     }
-
+//as
     public static void main(String[] args) {
         SayisalLoto loto = new SayisalLoto();
         for (int i=1;i<=10;i++) {
